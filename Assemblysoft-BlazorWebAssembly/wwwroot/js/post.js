d0ee14b0567b7de1932e219c1e7e2fc5412c366d
Carl Randall <crandall@myemsaccess.onmicrosoft.com>
2024-06-13 10:54:41 +0100

    feat: initial gratification, added assets + applied meta markup

---
+++ Assemblysoft-BlazorWebAssembly/wwwroot/js/post.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    // FitVids - start
+    var $postContent = $(".post-full-content");
+    $postContent.fitVids();
+    // FitVids - end
+
+    // Hover on avatar
+    var hoverTimeout;
+    $('.author-list-item').hover(function () {
+        var $this = $(this);
+
+        clearTimeout(hoverTimeout);
+
+        $('.author-card').removeClass('hovered');
+        $(this).children('.author-card').addClass('hovered');
+
+    }, function () {
+        var $this = $(this);
+
+        hoverTimeout = setTimeout(function () {
+            $this.children('.author-card').removeClass('hovered');
+        }, 800);
+    });
+});
---
+++ Assemblysoft-BlazorWebAssembly/wwwroot/js/typeinit.js
@@ -0,0 +1,13 @@
+﻿
+$(function () {
+    $(".typed").typed({
+        strings: [
+            "We <i class='fa fa-heart' style='color:white'></i> coffee <i class='fa fa-coffee'></i> ",
+            "We <i class='fa fa-heart' style='color:red'></i> people <i class='fa fa-users'></i> ",
+            "We love coding <i class='fa fa-code'></i> ",
+            "Problem solvers <i class='fa fa-code'></i> ",
+            "Translating Complex into Simple <i class='fa fa-cloud blue animated bounce'></i> "],
+        typeSpeed: 90
+    });
+});
+
